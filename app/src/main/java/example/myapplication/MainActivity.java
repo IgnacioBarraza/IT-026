85ba5d0e3a8dc0c30747dbe19ec1bc67e674ab6c
IgnacioBarraza <ignacio.barraza.rioja@gmail.com>
2024-10-30 17:06:57 -0300

    Add logic to calculate imc

--- app/src/main/java/example/myapplication/MainActivity.java
+++ app/src/main/java/example/myapplication/MainActivity.java
@@ -1,24 +1,46 @@
 package example.myapplication;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText et1, et2;
+    private TextView tv1, tv2;
+    private Button bt1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        et1 = (EditText) findViewById(R.id.weight);
+        et2 = (EditText) findViewById(R.id.height);
+        tv1 = (TextView) findViewById(R.id.imc);
+        tv2 = (TextView) findViewById(R.id.result);
+        bt1 = (Button) findViewById(R.id.calculate_imc);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imc();
+            }
         });
+    }
+
+    private void imc() {
+        double calculate = Integer.parseInt(et1.getText().toString()) / (Double.parseDouble(et2.getText().toString()) * Double.parseDouble(et2.getText().toString()));
+        tv1.setText(String.valueOf(Integer.parseInt(et1.getText().toString()) / (Double.parseDouble(et2.getText().toString()) * Double.parseDouble(et2.getText().toString()))));
+
+        if (calculate < 18.49) {
+            tv2.setText("Come más chamo");
+        } else if (calculate >= 18.49 && calculate <= 22.99) {
+            tv2.setText("Tas bien chamo");
+        } else if (calculate > 24.99) {
+            tv2.setText("Cierra el hocico chamo, tas waton");
+        }
     }
 }
